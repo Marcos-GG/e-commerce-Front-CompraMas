702c0dfaba4ed6e32866aa003da6023e97df873d
Marcos-GG <marcosdani4125@gmail.com>
2023-11-27 19:38:12 -0300

    tabla like

---
+++ server/src/models/Like/like.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "like",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primarykey: true,
+        autoIncrement: true,
+        validate: {
+          isInt: true,
+          min: 1,
+        },
+      },
+
+      userId: {
+        // id del usuario que le da like a la publicacion
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+
+      productId: {
+        // toma el id de la publicacion a la que le dieron like
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          isInt: true,
+          min: 1,
+        },
+      },
+    },
+    { timestamps: false }
+  );
+};

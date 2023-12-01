3a2a024520ecdd9d4a878646147821a4cbe82fc2
Marcos-GG <marcosdani4125@gmail.com>
2023-12-01 14:22:00 -0300

    get product se trae los comentarios con los datos del usurario

--- server/src/Controllers/Productos/getProduct.js
+++ server/src/Controllers/Productos/getProduct.js
@@ -1,15 +1,33 @@
-const { Products, Like, User } = require("../../db");
+const { Products, Like, User, Comment } = require("../../db");
 
 const getProductController = async (id) => {
   if (id) {
-    const idProduct = await Products.findByPk(id);
+    const idProduct = await Products.findByPk(id, {
+      include: [
+        {
+          model: Like,
+          include: { model: User, attributes: ["name", "lastname"] },
+        },
+        {
+          model: Comment,
+          include: { model: User, attributes: ["name", "lastname"] },
+        },
+      ],
+    });
 
     return idProduct;
   }
 
   const allProducts = await Products.findAll({
     include: [
-      { model: Like, include: { model: User, attributes: ["name", "id"] } },
+      {
+        model: Like,
+        include: { model: User, attributes: ["name", "lastname"] },
+      },
+      {
+        model: Comment,
+        include: { model: User, attributes: ["name", "lastname"] },
+      },
     ],
   });
 

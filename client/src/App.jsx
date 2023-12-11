3d32176d0e0bf7ee619fe9a3f3539ecbde56db85
Marcos-GG <marcosdani4125@gmail.com>
2023-12-11 16:53:03 -0300

    cualquier ruta que no se esncuentre definida te lleva a home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route /* useLocation  */ } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home/Home";
 import Admin from "./views/Admin/Admin";
 import DetailProduct from "./views/DetailProduct/DetailProduct";
 
 function App() {
-  // const location = useLocation()
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="admin" element={<Admin />} />
         <Route exact path="detail/:id" element={<DetailProduct />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );

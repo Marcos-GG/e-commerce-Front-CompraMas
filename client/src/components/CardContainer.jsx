b35fb97e41524baef302f0782a74ea55bd606a4b
Marcos-GG <marcosdani4125@gmail.com>
2024-01-14 03:40:59 -0300

    estilos en las card mas botones con iconos para vaoritos y like

--- client/src/components/CardContainer.jsx
+++ client/src/components/CardContainer.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable react/prop-types */
+import { Box } from "@mui/material";
 import Card from "./Card";
 
 function CardContainer({ products }) {
   return (
-    <div>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 2,
+      }}
+    >
       {products &&
         products.map((product) => (
           <div key={product?.id}>
             <Card product={product} />
           </div>
         ))}
-    </div>
+    </Box>
   );
 }
 

7b8e952931b66e6a197f943b31e7d2165b2b2af2
Marcos-GG <marcosdani4125@gmail.com>
2024-01-14 03:39:22 -0300

    logout te lleva a login

--- client/src/views/Logout/LogOut.jsx
+++ client/src/views/Logout/LogOut.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { LOGOUT } from "../../Redux/actionsTypes/LoginRegisterTypes";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function LogOut() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch({ type: LOGOUT });
     // localStorage.removeItem("carrito");
     localStorage.clear();
-  }, [dispatch]);
+    navigate("/login");
+  }, []);
 
   return <div>LogOut</div>;
 }

f597c65dcc965d3cbe66c6ffe6a13d6382fab937
Marcos-GG <marcosdani4125@gmail.com>
2024-01-15 22:02:20 -0300

    action product y actualizamos postProduct

--- server/src/Controllers/Productos/postProduct.js
+++ server/src/Controllers/Productos/postProduct.js
@@ -11,11 +11,11 @@ const postProdcutController = async (data) => {
   if (checkProductExist) throw new Error("El producto ya existe");
 
   // Obtener el nombre del género y la categoría usando sus IDs
-  const gender = await Gender.findByPk(data.gender);
-  const category = await Category.findByPk(data.category);
+  // const gender = await Gender.findByPk(data.gender);
+  // const category = await Category.findByPk(data.category);
 
-  data.gender = gender.gender;
-  data.category = category.name;
+  // data.gender = gender.gender;
+  // data.category = category.name;
 
   const newProduct = await Products.create(data);
 

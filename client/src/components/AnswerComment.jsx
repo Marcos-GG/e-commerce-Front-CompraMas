276588ec634266101a6f606212a8f46b50cdf48c
Marcos-GG <marcosdani4125@gmail.com>
2024-01-24 23:52:02 -0300

    utilizamos componentes de material para el input de Answer

--- client/src/components/AnswerComment.jsx
+++ client/src/components/AnswerComment.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { postAnswer, postAnswerAdmin } from "../Redux/actions/CommentsAction";
+import {
+  postAnswer /* postAnswerAdmin  */,
+} from "../Redux/actions/CommentsAction";
+import { Box, Button, TextField } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 // eslint-disable-next-line react/prop-types
 const AnswerComment = ({ commentId }) => {
@@ -28,25 +32,52 @@ const AnswerComment = ({ commentId }) => {
     }
 
     dispatch(postAnswer(form, commentId));
-    dispatch(postAnswerAdmin(form, commentId));
+    // dispatch(postAnswerAdmin(form, commentId));
 
     setForm({ answer: "" });
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(e);
+    }
+  };
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
+    <Box sx={{ width: "100%" }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          maxHeight: "3.5rem",
+          margin: "10px",
+          backgroundColor: "orange",
+        }}
+      >
+        {/* <Box
+          sx={{
+            width: "100%",
+            maxHeight: "3.5rem",
+            overflow: "hidden",
+          }}
+        > */}
+        <TextField
           type="text"
           name="answer"
           value={form.answer}
           onChange={formHandler}
+          onKeyPress={handleKeyPress}
+          multiline
+          maxRows={2}
+          fullWidth
+          sx={{ overflow: "hidden" }}
         />
-        <div>
-          <input type="submit" value="responder" />
-        </div>
+        {/* </Box> */}
+
+        <Button type="submit" variant="text" endIcon={<SendIcon />}></Button>
       </form>
-    </div>
+    </Box>
   );
 };
 

b04447093b5a69fa50499428867703be22138478
Marcos-GG <marcosdani4125@gmail.com>
2024-01-25 21:35:48 -0300

    estilos Navbar Admin

--- client/src/components/NavBarAdmin.jsx
+++ client/src/components/NavBarAdmin.jsx
@@ -1,32 +1,69 @@
+import { Box, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const NavBarAdmin = () => {
   return (
-    <>
-      <NavLink to="/">
-        <button> Home </button>
-      </NavLink>
+    <Box
+      sx={{
+        bgcolor: "#F5F5F5",
+        height: "3.2rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        boxShadow: "0px 5px 15px #888888;",
+      }}
+    >
+      <Box sx={{ display: "flex", gap: "15px", ml: "20px" }}>
+        <NavLink to="/">
+          <Button variant="contained" size="small">
+            {" "}
+            Home{" "}
+          </Button>
+        </NavLink>
 
-      <NavLink to="/admin">
-        <button> Comentarios </button>
-      </NavLink>
+        <NavLink to="/admin">
+          <Button variant="contained" size="small">
+            {" "}
+            Comentarios{" "}
+          </Button>
+        </NavLink>
 
-      <NavLink to="/admin/users">
-        <button> Usuarios </button>
-      </NavLink>
+        <NavLink to="/admin/users">
+          <Button variant="contained" size="small">
+            {" "}
+            Usuarios{" "}
+          </Button>
+        </NavLink>
 
-      <NavLink to="/admin/products">
-        <button> Productos </button>
-      </NavLink>
+        <NavLink to="/admin/products">
+          <Button variant="contained" size="small">
+            {" "}
+            Productos{" "}
+          </Button>
+        </NavLink>
 
-      <NavLink to="/admin/desactivatedProducts">
-        <button> Productos Desactivados</button>
-      </NavLink>
+        <NavLink to="/admin/desactivatedProducts">
+          <Button variant="contained" size="small">
+            {" "}
+            Productos Desactivados
+          </Button>
+        </NavLink>
 
-      <NavLink to="/admin/createProduct">
-        <button>Crear Producto</button>
-      </NavLink>
-    </>
+        <NavLink to="/admin/createProduct">
+          <Button variant="contained" size="small">
+            Crear Producto
+          </Button>
+        </NavLink>
+      </Box>
+
+      <Box sx={{ mr: "20px" }}>
+        <NavLink to="/logout">
+          <Button variant="contained" size="small">
+            LogOut
+          </Button>
+        </NavLink>
+      </Box>
+    </Box>
   );
 };
 

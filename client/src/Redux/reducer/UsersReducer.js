5a14bbb9c64d93eaf953b6aa5dea774dce026adb
Marcos-GG <marcosdani4125@gmail.com>
2024-01-29 21:39:33 -0300

    user reducer , actualizamos desde el front

--- client/src/Redux/reducer/UsersReducer.js
+++ client/src/Redux/reducer/UsersReducer.js
@@ -1,5 +1,9 @@
 import { POST_USERS } from "../actionsTypes/LoginRegisterTypes";
-import { GET_USERS } from "../actionsTypes/UsersActionTypes";
+import {
+  GET_USERS,
+  BLOCK_USER,
+  UNLOCK_USER,
+} from "../actionsTypes/UsersActionTypes";
 
 const initialState = {
   users: [],
@@ -15,6 +19,28 @@ const reducer = (state = initialState, action) => {
       return { ...state, users: [action.payload, ...state.users] };
     }
 
+    case BLOCK_USER: {
+      const updatedUser = state.users.map((user) => {
+        if (user.id === action.payload.id) {
+          return { ...user, active: 0 };
+        }
+        return user;
+      });
+
+      return { ...state, users: updatedUser };
+    }
+
+    case UNLOCK_USER: {
+      const updatedUser = state.users.map((user) => {
+        if (user.id === action.payload.id) {
+          return { ...user, active: 1 };
+        }
+
+        return user;
+      });
+
+      return { ...state, users: updatedUser };
+    }
     default:
       return { ...state };
   }

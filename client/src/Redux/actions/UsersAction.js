9ccbb1ead73284d00e6706c49f31c3c8257727b6
Marcos-GG <marcosdani4125@gmail.com>
2024-01-29 21:39:11 -0300

    action types y funciones para bloquear y desbloquear usuarios

--- client/src/Redux/actions/UsersAction.js
+++ client/src/Redux/actions/UsersAction.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { configureHeaders } from "../auth/configureHeaders";
 
-import { GET_USERS } from "../actionsTypes/UsersActionTypes";
+import {
+  GET_USERS,
+  UNLOCK_USER,
+  BLOCK_USER,
+} from "../actionsTypes/UsersActionTypes";
 
 export const getUsers = () => {
   return async function (dispatch) {
     try {
       const config = configureHeaders();
-      const response = await axios.get(`http://localhost:13050/users`, config);
+      const response = await axios.get(`http://localhost:55878/users`, config);
       const Users = response.data;
 
       const users = Users.map((user) => ({
@@ -20,6 +24,40 @@ export const getUsers = () => {
       dispatch({ type: GET_USERS, payload: users });
     } catch (error) {
       return error;
+    }
+  };
+};
+
+export const blockUser = (user) => {
+  return async function (dispatch) {
+    try {
+      const config = configureHeaders();
+      await axios.put(
+        `http://localhost:55878/users/${user.id}`,
+        { active: 0 },
+        config
+      );
+
+      dispatch({ type: BLOCK_USER, payload: user });
+    } catch (error) {
+      return { error: error.message };
+    }
+  };
+};
+
+export const unlockUser = (user) => {
+  return async function (dispatch) {
+    try {
+      const config = configureHeaders();
+      await axios.put(
+        `http://localhost:55878/users/${user.id}`,
+        { active: 1 },
+        config
+      );
+
+      dispatch({ type: UNLOCK_USER, payload: user });
+    } catch (error) {
+      return { error: error.message };
     }
   };
 };
--- client/src/Redux/actionsTypes/UsersActionTypes.js
+++ client/src/Redux/actionsTypes/UsersActionTypes.js
@@ -1,1 +1,3 @@
 export const GET_USERS = "GET_USERS";
+export const BLOCK_USER = "BLOCK_USER";
+export const UNLOCK_USER = "UNLOCK_USER";

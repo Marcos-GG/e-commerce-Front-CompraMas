50d87a004a160ad48383dd09b497909f9d1a3be6
Marcos-GG <marcosdani4125@gmail.com>
2024-02-02 03:10:54 -0300

    comment es un componente aparte

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import AnswerComment from "./AnswerComment";
 import UserAvatar from "./UserAvatar";
 import { jwtDecode } from "jwt-decode";
 import {
@@ -10,10 +10,14 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import FormatoHora from "./FormatoHora";
+import SelectedComment from "./SelectedComment";
+// import { useNavigate } from "react-router-dom";
 
 const Comments = ({ comments }) => {
+  // const navigate = useNavigate();
+
   const theme = useTheme();
   const isLessThanOrEqual430 = useMediaQuery(theme.breakpoints.down(430));
   const isLessThanOrEqual820 = useMediaQuery(theme.breakpoints.down(820));
@@ -27,13 +31,6 @@ const Comments = ({ comments }) => {
 
   const [selectedComment, setSelectedComment] = useState(null);
   const scrollContainerRef = useRef(null); // referencia al contenedor de scroll
-
-  useEffect(() => {
-    if (scrollContainerRef.current && selectedComment) {
-      scrollContainerRef.current.scrollTop =
-        scrollContainerRef.current.scrollHeight; // Ajustar la posición de desplazamiento al final del contenedor
-    }
-  }, [selectedComment]);
 
   return (
     <Box
@@ -86,6 +83,11 @@ const Comments = ({ comments }) => {
             }}
             onClick={() => {
               setSelectedComment(comment);
+
+              // {
+              //   isLessThanOrEqual430 &&
+              //     navigate(`/admin/comments/${comment.id}`);
+              // }
             }}
           >
             <UserAvatar user={comment.User} />
@@ -120,109 +122,16 @@ const Comments = ({ comments }) => {
       </Box>
       <Box>
         {selectedComment ? (
-          <Box
-            sx={{
-              width: isLessThanOrEqual430
-                ? "100vw"
-                : isLessThanOrEqual820
-                ? "25rem"
-                : isLessThanOrEqual980
-                ? "30rem"
-                : isLessThanOrEqual1268
-                ? "40rem"
-                : isLessThanOrEqual1662
-                ? "50rem"
-                : "68rem",
-              height: isLessThanOrEqual430 ? `calc(100vh - 3.2rem)` : "45rem",
-              borderRadius: isLessThanOrEqual430 ? "" : " 0 20px 20px 0 ",
-              backgroundColor: "#f5f5f5",
-              boxShadow: "10px 10px 15px #888888;",
-              backgroundImage: 'url("/logoblanco.svg")',
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <Box>
-              <Box
-                ref={scrollContainerRef}
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  height: "43rem",
-                  maxHeight: isLessThanOrEqual430 ? "" : "40rem", // Establecer la altura máxima para activar el scroll
-                  overflowY: "auto", // Habilitar el scroll vertical cuando el contenido excede la altura máxima
-                }}
-              >
-                <Typography
-                  sx={{
-                    width: isLessThanOrEqual430
-                      ? "17rem"
-                      : isLessThanOrEqual1268
-                      ? "18rem"
-                      : "24rem",
-                    borderRadius: "0 15px 15px 15px",
-                    margin: "15px",
-                    // margin: "4px",
-                    padding: "10px",
-                    border: "1px solid #00CCFD",
-                    bgcolor: "#F5F5F5",
-                    boxShadow: "10px 10px 15px #888888;",
-                  }}
-                >
-                  {selectedComment.text}
-                </Typography>
-
-                {selectedComment.Answers.map((answer) => (
-                  <Box
-                    key={answer.id}
-                    sx={{
-                      display: "flex",
-                      justifyContent:
-                        userId === answer.userId ? "flex-end" : "flex-start",
-                    }}
-                  >
-                    <Box
-                      sx={{
-                        display: "flex",
-                        bgcolor:
-                          userId === answer.userId ? "#00CCFD" : "#F5F5F5",
-                        width: "24rem",
-                        borderRadius:
-                          userId === answer.userId
-                            ? "15px 15px 0px 15px;"
-                            : "0 15px 15px 15px",
-                        mx: "15px",
-                        justifyContent: "center",
-                        padding: "10px",
-                        my: "5px",
-                        boxShadow: "10px 10px 15px #888888;",
-                      }}
-                    >
-                      <Typography
-                        sx={{
-                          width: "100%",
-                          display: "flex",
-                          flexWrap: "wrap",
-                          boxSizing: "border-box",
-                        }}
-                      >
-                        {answer.answer}
-                      </Typography>
-                    </Box>
-                  </Box>
-                ))}
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                }}
-              >
-                <AnswerComment commentId={selectedComment.id} />
-              </Box>
-            </Box>
-          </Box>
+          <SelectedComment
+            selectedComment={selectedComment}
+            isLessThanOrEqual430={isLessThanOrEqual430}
+            isLessThanOrEqual820={isLessThanOrEqual820}
+            isLessThanOrEqual980={isLessThanOrEqual980}
+            isLessThanOrEqual1268={isLessThanOrEqual1268}
+            isLessThanOrEqual1662={isLessThanOrEqual1662}
+            scrollContainerRef={scrollContainerRef}
+            userId={userId}
+          />
         ) : (
           <Box
             sx={{
@@ -239,8 +148,7 @@ const Comments = ({ comments }) => {
                 ? "50rem"
                 : "68rem",
               height: "45rem",
-
-              borderRadius: " 0 20px 20px 0 ",
+              borderRadius: "0 20px 20px 0",
               backgroundColor: "#F5F5F5",
               boxShadow: "10px 10px 15px #888888",
               backgroundImage: 'url("/logoblanco.svg")',

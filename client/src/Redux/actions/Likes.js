3d6b9bc5fc7cd6b006e468eadcb4229310052064
Marcos-GG <marcosdani4125@gmail.com>
2024-02-03 10:17:19 -0300

    get de likes desde el front

--- client/src/Redux/actions/Likes.js
+++ client/src/Redux/actions/Likes.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import { ADD_LIKE, REMOVE_LIKE } from "../actionsTypes/LikesTypes";
 import { configureHeaders } from "../auth/configureHeaders";
+import { SET_FAVORITES } from "../actionsTypes/ProductsActionTypes";
 
 export const addLike = (productId) => {
   return async function (dispatch) {
@@ -30,5 +31,20 @@ export const removeLike = (productId) => {
     console.log(response.data, "estructura del removelike");
 
     dispatch({ type: REMOVE_LIKE, payload: response.data });
+  };
+};
+
+export const getFavorites = () => {
+  return async function (dispatch) {
+    const config = configureHeaders();
+    const response = await axios.get(
+      "http://localhost:55878/likes/getFavorites",
+      config
+    );
+
+    console.log(response.data, "estructura del like");
+    const ProductosFavoritos = response.data.map((like) => like.Product);
+
+    dispatch({ type: SET_FAVORITES, payload: ProductosFavoritos });
   };
 };

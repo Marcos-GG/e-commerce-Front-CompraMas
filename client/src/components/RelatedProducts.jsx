7e47430b0fcf6a39f020e98db69ca8729b1fa740
Marcos-GG <marcosdani4125@gmail.com>
2024-02-08 15:10:44 -0300

    se renderiza y funciona pero tiene un bug al volver hacia atras . se empezo el estilo de detail

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,23 +1,59 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import { getProductId } from "../Redux/actions/productsActions";
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 
-function RelatedProducts({ limitedFilteredProducts }) {
+function RelatedProducts({ products, category, gender }) {
+  const { id } = useParams();
+  console.log(id, "id");
+  console.log(products, "products");
+  console.log(category, "category");
+  console.log(gender, "gender");
+
+  const dispatch = useDispatch();
+
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    // Filtrar directamente en el useEffect
+    const newFilteredProducts = products
+      .filter((product) => {
+        return (
+          product.id != id && // Excluir el producto actual
+          product.category === category &&
+          product.gender === gender
+        );
+      })
+      .slice(0, 5);
+
+    // Actualizar el estado
+    setFilteredProducts(newFilteredProducts);
+  }, [id, products, category, gender]);
+
+  const handleProductClick = (productId) => {
+    dispatch(getProductId(productId));
+  };
+
   return (
-    <>
+    <Box>
       <h3>Productos relacionados:</h3>
       <div>
-        {Array.isArray(limitedFilteredProducts) &&
-          limitedFilteredProducts.map((product) => (
-            <div key={product.id}>
-              <NavLink to={`/detail/${product.id}`}>
-                <p>{product?.image}</p>
-                <p>{product?.price}</p>
-                <button>Ver producto</button>
-              </NavLink>
-            </div>
-          ))}
+        {filteredProducts.map((product) => (
+          <div key={product.id}>
+            <NavLink to={`/detail/${product.id}`}>
+              <img src={product?.image} style={{ width: "150px" }}></img>
+              <p>{product?.price}</p>
+              <button onClick={() => handleProductClick(product.id)}>
+                Ver producto
+              </button>
+            </NavLink>
+          </div>
+        ))}
       </div>
-    </>
+    </Box>
   );
 }
 
--- client/src/views/DetailProduct/DetailProduct.jsx
+++ client/src/views/DetailProduct/DetailProduct.jsx
@@ -1,21 +1,33 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getProductId } from "../../Redux/actions/productsActions";
+import { getProductId, getProducts } from "../../Redux/actions/productsActions";
 import { useEffect } from "react";
 import RelatedProducts from "../../components/RelatedProducts";
 import CommetProducts from "../../components/CommentProducts";
 import AnswerComment from "../../components/AnswerComment";
 import { addProduct } from "../../Redux/actions/ShoppingCartAction";
 import { jwtDecode } from "jwt-decode";
+import { Box, Button, Typography } from "@mui/material";
+import ProductPrice from "../../components/ProductPrice";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 function DetailProduct() {
   const { id } = useParams();
   const dispatch = useDispatch();
 
   const productId = useSelector((state) => state.products.productId);
-  console.log(productId, "se acutliza el answer desde el detail");
   const products = useSelector((state) => state.products.products);
 
+  /// productos relacionados
+  const category = productId?.category;
+  const gender = productId?.gender;
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch, id]);
+
+  ///////
   const token = localStorage.getItem("token");
   const isAdmin = localStorage.getItem("admin");
 
@@ -27,49 +39,156 @@ function DetailProduct() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // productos relacionados :
-  const category = productId?.category;
-  const gender = productId?.gender;
-
-  const filterProducts = products.filter((product) => {
-    return product
-      ? product.id !== id &&
-          product.category === category &&
-          product.gender === gender
-      : product.gender === gender && product.id !== id;
-  });
-
   const handleClickAdd = (product) => {
     dispatch(addProduct(product));
   };
 
-  const limitedFilteredProducts = filterProducts.slice(0, 5);
+  const priceStyle = {
+    fontSize: "2.2rem",
+  };
 
   return (
-    <>
-      <h2>Hola soy detail del producto</h2>
-      <div>
-        <div key={productId?.id}>
-          <p>id : {productId?.id}</p>
-          <p>title: {productId?.title}</p>
-          <img src={productId?.image}></img>
-          <p>description:{productId?.description}</p>
-          <p>price: {productId?.price}</p>
-          <p>gender: {productId?.gender}</p>
-          <p>category: {productId?.category}</p>
-          <p>likes: {productId?.likes}</p>
+    <Box>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center", // Alinea horizontalmente al centro
+            mx: "5rem",
+            mt: "3rem",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              width: "240rem",
+              flexDirection: "column",
+              height: "",
+              justifyContent: "center", // Alinea horizontalmente al centro
+              alignItems: "center",
+            }}
+          >
+            {/* <Box sx={{ alignSelf: "flex-start" }}>
+              <Typography
+                variant="h5"
+                sx={{
+                  fontFamily: "Josefin Sans, sans-serif",
+                  fontStyle: "italic",
+                  transform: "skewX(-15deg)",
+                  textTransform: "uppercase",
+                }}
+              >
+                {productId?.category}
+              </Typography>
+            </Box> */}
+            <img
+              src={productId.image}
+              alt={productId.title}
+              style={{ width: "430px", borderRadius: "5px" }} // Estilos directos a la imagen
+            />
+          </Box>
 
-          <div>
-            <button onClick={() => handleClickAdd(productId)}>Agregar</button>
-          </div>
-        </div>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                maxwidth: "65rem",
+              }}
+            >
+              <Box sx={{ alignSelf: "flex-end" }}>
+                <Typography
+                  sx={{
+                    display: "flex",
+                    fontSize: "13px",
+                    fontWeight: "bold",
+                    color: "gray",
+                    fontStyle: "italic",
+                  }}
+                >
+                  {<FavoriteBorderIcon fontSize="small" />}
+                  {productId.likes} veces indicado como favorito
+                </Typography>{" "}
+              </Box>
+
+              <Box sx={{ width: "80%", height: "100%" }}>
+                <Typography
+                  variant="h2"
+                  sx={{ textTransform: "uppercase", letterSpacing: "0.2em" }}
+                >
+                  {productId?.title}
+                </Typography>
+
+                <Typography component="p" sx={{ ml: "15px", my: "5px" }}>
+                  {productId?.category} para {productId?.gender}
+                </Typography>
+
+                <Typography variant="h4" mt={2.5}>
+                  <ProductPrice price={productId?.price} style={priceStyle} />
+                </Typography>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{
+                      width: "12rem",
+                      mt: "5px",
+                      borderRadius: "40px",
+                      fontSize: "18px",
+                      mb: "10px",
+                    }}
+                  >
+                    COMPRAR
+                  </Button>
+                  <Button
+                    variant="contained"
+                    endIcon={<AddShoppingCartIcon />}
+                    sx={{
+                      width: "13rem",
+                      height: "2.5rem",
+                      borderRadius: "40px",
+                      fontSize: "13px",
+                      mb: "30px",
+                    }}
+                    onClick={() => handleClickAdd(productId)}
+                  >
+                    Agregar al carrito
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+
+            <Box>
+              <Typography
+                component="p"
+                sx={{ fontSize: "16px", width: "75%", mx: "auto" }}
+              >
+                {productId?.description}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+
         {productId.Comments && productId.Comments.length > 0 ? (
-          <div style={{ backgroundColor: "green" }}>
+          <Box style={{ backgroundColor: "green" }}>
             <h3>Comentarios:</h3>
 
             {productId.Comments.map((comment) => (
-              <div key={comment.id}>
-                <div
+              <Box key={comment.id}>
+                <Box
                   style={{
                     margin: "10px",
                     backgroundColor: "aqua",
@@ -78,7 +197,7 @@ function DetailProduct() {
                 >
                   <p>{comment.text}</p>
 
-                  <div style={{ backgroundColor: "yellow" }}>
+                  <Box style={{ backgroundColor: "yellow" }}>
                     <p>Respuestas:</p>
                     {comment.Answers && comment.Answers.length > 0 ? (
                       comment.Answers.map((answer) => (
@@ -93,28 +212,31 @@ function DetailProduct() {
                     ) : (
                       <p>No hay respuestas para este comentario.</p>
                     )}
-                  </div>
+                  </Box>
                   {(isAdmin || userId === comment.userId) && (
                     <AnswerComment commentId={comment.id} />
                   )}
-                </div>
-              </div>
+                </Box>
+              </Box>
             ))}
-          </div>
+          </Box>
         ) : (
           <p>No hay comentarios para este producto.</p>
         )}
-      </div>
-      <div>
+      </Box>
+      <Box>
         <h3>generar comentario</h3>
         <CommetProducts productId={productId?.id} />
-      </div>
-      <div>
-        {limitedFilteredProducts.length > 0 && (
-          <RelatedProducts limitedFilteredProducts={limitedFilteredProducts} />
-        )}
-      </div>
-    </>
+      </Box>
+      <Box>
+        <RelatedProducts
+          id={id}
+          products={products}
+          category={category}
+          gender={gender}
+        />
+      </Box>
+    </Box>
   );
 }
 

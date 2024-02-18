f434ad9a41a84b5d05b1736abdb03cc878f0492c
Marcos-GG <marcosdani4125@gmail.com>
2024-02-18 15:18:31 -0300

    productos relacionados totalmente terminado

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useDispatch } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getProductId } from "../Redux/actions/productsActions";
 import { useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Typography,
+  keyframes,
+  styled,
+  useMediaQuery,
+} from "@mui/material";
+import ProductItem from "./ProductItem";
 
 function RelatedProducts({ products, category, gender }) {
-  const { id } = useParams();
-  console.log(id, "id");
-  console.log(products, "products");
-  console.log(category, "category");
-  console.log(gender, "gender");
+  const isLTE426 = useMediaQuery("(max-width:426px)");
+  const isLTE800 = useMediaQuery("(max-width:800px)");
+  const isLTE1250 = useMediaQuery("(max-width:1250px)");
 
+  const navigate = useNavigate();
+  const { id } = useParams();
   const dispatch = useDispatch();
 
   const [filteredProducts, setFilteredProducts] = useState([]);
@@ -37,22 +44,111 @@ function RelatedProducts({ products, category, gender }) {
     dispatch(getProductId(productId));
   };
 
+  const priceStyle = {
+    fontSize: "18px",
+    fontWeight: "bold",
+    color: "black",
+  };
+
+  const slideAnimation = keyframes`
+  0% { transform: translateX(0); }
+  100% { transform: translateX(calc(-11.6rem * ${filteredProducts.length} ));
+}
+`;
+
+  const slideAnimationSmallScreen = keyframes`
+  0% { transform: translateX(0); }
+  100% { 
+    transform: translateX(calc(-10.5rem * ${filteredProducts.length} ));
+  }
+`;
+
+  const AnimatedBox = styled(Box)({
+    display: "flex",
+    overflow: "hidden",
+    position: "relative",
+    "& > div": {
+      whiteSpace: "nowrap",
+      display: "flex",
+      animation: `${slideAnimation} 30s linear infinite`, // Aquí he aumentado la duración para mantener el desplazamiento continuo
+    },
+    "@media (max-width: 800px)": {
+      "& > div": {
+        animation: `${slideAnimationSmallScreen} 30s linear infinite`, // Para pantallas de 800px o menos
+      },
+    },
+  });
+
   return (
     <Box>
-      <h3>Productos relacionados:</h3>
-      <div>
-        {filteredProducts.map((product) => (
-          <div key={product.id}>
-            <NavLink to={`/detail/${product.id}`}>
-              <img src={product?.image} style={{ width: "150px" }}></img>
-              <p>{product?.price}</p>
-              <button onClick={() => handleProductClick(product.id)}>
-                Ver producto
-              </button>
-            </NavLink>
-          </div>
-        ))}
-      </div>
+      {filteredProducts && filteredProducts?.length > 0 && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: isLTE1250 ? "center" : "start",
+          }}
+        >
+          <Typography
+            variant="h5"
+            sx={{ m: "20px 0 10px 0", alignSelf: "center" }}
+          >
+            Productos relacionados:
+          </Typography>
+
+          <Box sx={{ position: "relative", width: "90%" }}>
+            <AnimatedBox sx={{ width: "100%" }}>
+              {filteredProducts?.length > 0 &&
+                filteredProducts.map((product) => (
+                  <Box key={product.id} sx={{ width: "100%", mx: "3px" }}>
+                    <ProductItem
+                      product={product}
+                      navigate={navigate}
+                      priceStyle={priceStyle}
+                      handleProductClick={handleProductClick}
+                    />
+                  </Box>
+                ))}
+              {filteredProducts?.length > 0 &&
+                filteredProducts.map((product) => (
+                  <Box key={product.id} sx={{ width: "100%", mx: "3px" }}>
+                    <ProductItem
+                      product={product}
+                      navigate={navigate}
+                      priceStyle={priceStyle}
+                      handleProductClick={handleProductClick}
+                    />
+                  </Box>
+                ))}
+            </AnimatedBox>
+
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: -1, // Ajusta según tus necesidades
+                height: "100%",
+                width: isLTE426 ? 40 : isLTE800 ? 100 : 200, // Ancho del efecto de desvanecimiento
+                background:
+                  "linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))",
+              }}
+            />
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: -2, // Ajusta según tus necesidades
+                height: "100%",
+                width: isLTE426 ? 40 : isLTE800 ? 100 : 200, // Ancho del efecto de desvanecimiento
+                background:
+                  "linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))",
+              }}
+            />
+          </Box>
+          {/* Pseudo-elementos para el efecto de desvanecimiento */}
+        </Box>
+      )}
     </Box>
   );
 }

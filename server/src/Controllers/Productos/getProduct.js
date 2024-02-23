7920f4b24f5221c3458ee5f9795a8b41f77b140b
Marcos-GG <marcosdani4125@gmail.com>
2024-02-23 20:38:59 -0300

    handler y controller para filtrar por terminos

--- server/src/Controllers/Productos/getProduct.js
+++ server/src/Controllers/Productos/getProduct.js
@@ -1,6 +1,7 @@
+const { Op } = require("sequelize");
 const { Products, Like, User, Comment, Answer } = require("../../db");
 
-const getProductController = async (id) => {
+const getProductController = async (id, search) => {
   if (id) {
     const idProduct = await Products.findByPk(id, {
       include: [
@@ -25,6 +26,71 @@ const getProductController = async (id) => {
     });
 
     return idProduct;
+  }
+
+  // separamos el valor de search
+  const searchTerms = search ? search.split(" ") : [];
+  const stopWords = [
+    "del",
+    "de",
+    "el",
+    "ella",
+    "desde",
+    "como",
+    "pero",
+    "la",
+    "otro",
+    "ellos",
+    "ellas",
+    "esos",
+    "cómo",
+    "donde",
+    "e",
+    "y",
+    "a",
+    "por",
+    "que",
+    "su",
+    "se",
+    "un",
+    "una",
+    "son",
+    "tienen",
+    "tienen",
+    "tu",
+    "tus",
+    "unos",
+  ];
+
+  const filterSearchTerms = searchTerms.filter(
+    (term) => !stopWords.includes(term.toLowerCase())
+  );
+
+  const palabras = filterSearchTerms.map((term) => ({
+    [Op.or]: [
+      {
+        title: {
+          [Op.like]: `%${term}%`,
+        },
+      },
+      {
+        description: {
+          [Op.like]: `%${term}%`,
+        },
+      },
+    ],
+  }));
+
+  // Op.and es el operador que permite combinar esas condiciones para formar una sola condición lógica. todas tienen que ser true ( en este caso las condiciones de palabras )
+
+  if (search) {
+    const products = await Products.findAll({
+      where: {
+        [Op.and]: palabras,
+      },
+    });
+
+    return products;
   }
 
   const allProducts = await Products.findAll({
--- server/src/Handlers/Productos/getProduct.js
+++ server/src/Handlers/Productos/getProduct.js
@@ -3,16 +3,27 @@ const getProductController = require("../../Controllers/Productos/getProduct");
 const getProductHandler = async (req, res) => {
   try {
     const { id } = req.params;
+    const { search } = req.query;
 
     if (id) {
-      const idProduct = await getProductController(id);
-      if (!idProduct) throw new Error("El producto no existe");
+      const idProduct = await getProductController(id, search);
+      if (!idProduct) throw new Error("El producto no existe.");
 
       return res.status(200).json(idProduct);
     }
 
+    if (search) {
+      const searchProducts = await getProductController(id, search);
+      if (!searchProducts)
+        throw new Error(
+          "No se encontraron productos con la información ingresada."
+        );
+
+      return res.status(200).json(searchProducts);
+    }
+
     const allProducts = await getProductController();
-    if (allProducts.length === 0) throw new Error("No existen productos");
+    if (allProducts.length === 0) throw new Error("No existen productos.");
 
     return res.status(200).json(allProducts);
   } catch (error) {

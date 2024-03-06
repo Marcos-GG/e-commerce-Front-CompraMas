ce470286417ae74a0d6f08fb1171cffdd5e755fa
Marcos-GG <marcosdani4125@gmail.com>
2024-03-06 20:53:09 -0300

    sacamos post comennts y pos answer por ahora estan dentro de products

--- client/src/Redux/reducer/commentsReducer.js
+++ client/src/Redux/reducer/commentsReducer.js
@@ -1,8 +1,4 @@
-import {
-  ALL_COMMENTS,
-  POST_COMMENTS,
-  POST_ANSWER_COMMENT_ID,
-} from "../actionsTypes/CommentsTypes.js";
+import { ALL_COMMENTS } from "../actionsTypes/CommentsTypes.js";
 
 const initialState = {
   comments: [],
@@ -12,29 +8,6 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ALL_COMMENTS: {
       return { ...state, comments: action.payload }; // Reemplaza los comentarios existentes con los nuevos
-    }
-
-    case POST_COMMENTS: {
-      return { ...state, comments: [action.payload, ...state.comments] }; // Agrega el nuevo comentario al principio de la lista
-    }
-
-    case POST_ANSWER_COMMENT_ID: {
-      const commentId = action.payload.commentId;
-      // Actualiza los comentarios con la nueva respuesta
-      const updatedComments = state.comments.map((comment) => {
-        if (comment.id === commentId) {
-          return {
-            ...comment,
-            Answers: [...comment.Answers, action.payload],
-          };
-        }
-        return comment;
-      });
-
-      return {
-        ...state,
-        comments: updatedComments,
-      };
     }
 
     default:

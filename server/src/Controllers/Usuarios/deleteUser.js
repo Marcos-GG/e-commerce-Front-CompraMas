7b6115c4965cafa35eaa9b7551cbf13fd4c36f0d
Marcos-GG <marcosdani4125@gmail.com>
2024-03-14 14:01:31 -0300

    se elimina correctamtne el usaurio eliminando primero los comentarios y answers que tenga el usuario con productos y luego lo elimina

--- server/src/Controllers/Usuarios/deleteUser.js
+++ server/src/Controllers/Usuarios/deleteUser.js
@@ -1,7 +1,23 @@
-const { User } = require("../../db");
+const { User, Comment } = require("../../db");
 
 const deleteUserController = async (id) => {
-  const user = await User.destroy({ where: { id: id } });
+  const user = await User.findByPk(id);
+
+  if (user) {
+    const comentariosAsociados = await Comment.findAll({
+      where: { userId: user.id },
+    });
+
+    if (comentariosAsociados.length > 0) {
+      await Comment.destroy({
+        where: { userId: user.id },
+      });
+    }
+
+    await User.destroy({
+      where: { id: id },
+    });
+  }
 
   if (!user) {
     return { deletedRows: 0, message: "User not found" };

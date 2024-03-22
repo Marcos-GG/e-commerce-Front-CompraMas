1139c2594df7b62ecc1cbc3a12830d55217f7455
Marcos-GG <marcosdani4125@gmail.com>
2024-03-22 01:18:53 -0300

    getProductAll y circularProgress

--- client/src/views/Admin/Admin.jsx
+++ client/src/views/Admin/Admin.jsx
@@ -4,48 +4,23 @@ import Comments from "../../components/Comments";
 import { useDispatch, useSelector } from "react-redux";
 import { allComments } from "../../Redux/actions/CommentsAction";
 import { Box } from "@mui/material";
-import { GET_PRODUCTS } from "../../Redux/actionsTypes/ProductsActionTypes";
-import { getProducts } from "../../Redux/actions/productsActions";
+
 import Circularprogress from "../../components/CircularProgress";
+import { getProductsAll } from "../../Redux/actions/productsActions";
 
 const Admin = () => {
   const dispatch = useDispatch();
   const AllComments = useSelector((state) => state.comments.comments);
 
   useEffect(() => {
-    try {
-      const persistedData = localStorage.getItem("persist:root");
-
-      if (persistedData) {
-        const parsedData = JSON.parse(persistedData);
-
-        const localProducts =
-          parsedData.products && JSON.parse(parsedData.products).products;
-
-        if (localProducts || localProducts.length > 0) {
-          dispatch({ type: GET_PRODUCTS, payload: localProducts });
-        }
-      }
-
-      dispatch(getProducts());
-    } catch (error) {
-      return error;
-    }
-  }, []);
-
-  useEffect(() => {
     dispatch(allComments());
+    dispatch(getProductsAll());
   }, []);
-
-  const [comments, setComments] = useState(null);
-
-  useEffect(() => {
-    setComments(AllComments);
-  }, [AllComments]);
 
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    if (comments !== null) {
+    if (AllComments !== null) {
       setIsLoading(false);
     } else {
       setIsLoading(true);
@@ -71,7 +46,7 @@ const Admin = () => {
               zIndex: -1,
             }}
           />
-          <Comments comments={comments} />
+          <Comments comments={AllComments} />
         </Box>
       )}
     </Box>
@@ -79,21 +54,3 @@ const Admin = () => {
 };
 
 export default Admin;
-// useEffect(() => {
-//   const persistedData = localStorage.getItem("persist:root");
-
-//   if (persistedData) {
-//     const parsedData = JSON.parse(persistedData);
-
-//     const localComments =
-//       parsedData.comments && JSON.parse(parsedData.comments).comments;
-
-//     if (localComments || localComments.length > 0) {
-//       if (!comments || comments.length === 0) {
-//         dispatch({ type: ALL_COMMENTS, payload: localComments });
-//       }
-//     }
-//   }
-
-//   dispatch(allComments());
-// }, []);

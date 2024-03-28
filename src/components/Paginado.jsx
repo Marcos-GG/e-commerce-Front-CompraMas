e0a072f2cb054de42c903297b3c88fa7557dd72d
Marcos-GG <marcosdani4125@gmail.com>
2024-03-28 02:39:43 -0300

    scroll top y circularProgress mientras pasamos de pagina

--- src/components/Paginado.jsx
+++ src/components/Paginado.jsx
@@ -2,6 +2,7 @@ import { Box, Pagination } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { apllyFilters, getProducts } from "../Redux/actions/productsActions";
 import { useEffect, useState } from "react";
+import Circularprogress from "./CircularProgress";
 
 const Paginado = () => {
   const dispatch = useDispatch();
@@ -25,6 +26,20 @@ const Paginado = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+
+    window?.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [currentPage]);
+
   const handlePageChange = (page) => {
     setCurrentPage(page);
     if (EstaFiltrandoSearch || EstaFiltrando) {
@@ -35,16 +50,18 @@ const Paginado = () => {
   };
 
   useEffect(() => {
+    // Resetear la página cuando cambie la lista de productos
     setCurrentPage(1);
   }, [paginadoFiltrados]);
 
   return (
     <Box sx={{ bgcolor: "#F5F5F5", my: "2rem" }}>
+      {isLoading && <Circularprogress />}
       <Pagination
         count={Math.ceil(
           (EstaFiltrando || EstaFiltrandoSearch
             ? paginadoFiltrados
-            : productosLength) / 21
+            : productosLength) / 7
         )}
         page={currentPage}
         onChange={(e, page) => {
